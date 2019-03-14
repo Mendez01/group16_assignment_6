5d4372b95e464e62b81de31a527f0dcd3273cd0b
Thomas Mendez <thomasmendez@utexas.edu>
2019-03-14 17:54:46 -0500

    Setup

---
+++ group16_assignment6/group16_assignment6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  group16_assignment6
+//
+//  Created by Thomas Mendez on 3/14/19.
+//  Copyright © 2019 group16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

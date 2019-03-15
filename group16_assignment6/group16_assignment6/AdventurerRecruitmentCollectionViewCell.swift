c920ef87bdeb34e8d976f1f81698f18aedd88e3e
Khalid Alkhatib <alkhatibkhalid@utexas.edu>
2019-03-15 01:53:09 -0500

    Khalid change 1

---
+++ group16_assignment6/group16_assignment6/AdventurerRecruitmentCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AdventurerRecruitmentCollectionViewCell.swift
+//  group16_assignment6
+//
+//  Created by Khalid Alkhatib on 3/15/19.
+//  Copyright © 2019 group16. All rights reserved.
+//
+
+import UIKit
+
+class AdventurerRecruitmentCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var image: UIImageView!
+}
